4815bdc2b2598b9906c36ca0ece4e32165782047
AlbertoRochaPinalli <AlbertoRochaPinalli@9.85.232.197>
2019-09-23 15:01:15 -0300

    Criada a class Conta

---
+++ src/main/java/br/com/pinalli/conta/TesteReferencia.java
@@ -0,0 +1,15 @@
+package br.com.pinalli.conta;
+
+/**
+ *
+ * @author AlbertoRochaPinalli
+ */
+public class TesteReferencia {
+    public static void main(String[] args) {
+        
+        Conta c = new Conta();
+        c.saldo = 200;
+        
+        System.out.println("Saldo de c "+ c.saldo);
+    }
+}
